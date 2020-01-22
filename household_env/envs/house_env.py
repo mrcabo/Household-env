7d1140ce719e64e09ec56713bf270892317c0a1d
mrcabo <dcgdiego@gmail.com>
2020-01-22 17:43:58 +0100

    Necessary functions

--- household_env/envs/house_env.py
+++ household_env/envs/house_env.py
@@ -2,6 +2,23 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 
+# A good example is https://github.com/openai/gym-soccer
+
 
 class HouseholdEnv(gym.Env):
     metadata = {'render.modes': ['human']}
+
+    def __init__(self):
+        pass
+
+    def step(self, action):
+        pass
+
+    def reset(self):
+        pass
+
+    def render(self, mode='human'):
+        pass
+
+    def close(self):
+        pass
