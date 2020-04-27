f6499322cfe95fad1bdd296f5d3debc6e573c1dd
mrcabo <dcgdiego@gmail.com>
2020-04-27 16:12:41 +0200

    Project can be uploaded to PyPi

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
 from setuptools import setup
 
-setup(name='gym_foo',
-      version='0.0.1',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='household_env',
+      version='0.0.2',
       install_requires=['gym', 'numpy'],  # And any other dependencies foo needs
       package_data={
           # If any package contains *.txt or *.rst files, include them:
           "": ["*.txt", "*.json"]
+      },
+      # metadata to display on PyPI
+      author="Diego Cabo Golvano",
+      author_email="dcgdiego@gmail.com",
+      description="This is an environment for RL purposes",  # TODO
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      keywords="Reinforcement Learning environment",
+      url="https://github.com/mrcabo/Household-env.git",  # project home page, if any
+      project_urls={
+          "Source Code": "https://github.com/mrcabo/Household-env.git",
       }
       )
