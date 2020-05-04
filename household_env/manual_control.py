b1c5429a34ff90006706943b4b30c5e5fdc414de
mrcabo <dcgdiego@gmail.com>
2020-05-04 15:48:16 +0200

    Only returns position. ep. lenght set to 50

--- household_env/manual_control.py
+++ household_env/manual_control.py
@@ -9,15 +9,17 @@ env = gym.make('household_env:Household-v0')
 env.set_current_task(tasks_list[0])
 x = 0
 while x != -1:
-    env.reset()
+    state = env.reset()
     env.render()
     done = False
     while not done:
+        print(f"Current state: {state}")
         x = int(input("Enter your command: "))
         if x < 0 or x > 8:
             print("Input should be between 0 and 8")
             break
         next_state, reward, done, info = env.step(x)  # take a random action
+        state = next_state
         env.render()
         print(f"next_state: {next_state}, Reward: {reward}, Done: {done}")
     print("End of episode")
