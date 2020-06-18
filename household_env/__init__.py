4777b316e31f2946e6e3574008008ae5a66e9376
mrcabo <dcgdiego@gmail.com>
2020-06-18 13:54:10 +0200

    Increasing max number of timesteps to 1000

--- household_env/__init__.py
+++ household_env/__init__.py
@@ -3,6 +3,6 @@ from gym.envs.registration import register
 register(
     id='Household-v0',
     entry_point='household_env.envs:HouseholdEnv',
-    max_episode_steps=200,
+    max_episode_steps=1000,
     reward_threshold=95,
 )
